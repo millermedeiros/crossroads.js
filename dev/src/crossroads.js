81176d5988676c043d3c876a1af60204df4108f1
millermedeiros <miller@millermedeiros.com>
2011-04-10 23:27:17 -0300

    added core build tasks and wrappers

---
+++ dev/src/crossroads.js
@@ -0,0 +1,3 @@
+/*jslint onevar:true, undef:true, newcap:true, regexp:true, bitwise:true, maxerr:50, indent:4, white:false, nomen:false, plusplus:false */
+
+	var crossroads = {};
---
+++ dev/src/wrapper_closure.js
@@ -0,0 +1,11 @@
+/*jslint onevar:true, undef:true, newcap:true, regexp:true, bitwise:true, maxerr:50, indent:4, white:false, nomen:false, plusplus:false */
+/*global window:false*/
+
+//::LICENSE:://
+(function(global){
+	
+	//::CROSSROADS_JS:://
+	
+	global.crossroads = crossroads;
+	
+}(window || this));
