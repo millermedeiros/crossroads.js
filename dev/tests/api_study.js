eddfa35a4ddb505abea1ae9ff3af3943e450c23b
millermedeiros <miller@millermedeiros.com>
2011-04-13 00:57:47 -0300

    added a few more api proposals and some comments as well

--- dev/tests/api_study.js
+++ dev/tests/api_study.js
@@ -11,7 +11,7 @@
 //==== add new route ====//
 
 
-//01 - config object + callback 
+//01 - config object + callback => ugly and unorganized
 
 //params => {pattern:String, callback:Function, [id]:String, [rules]:Object, [separator]:String}
 crossroads.add({
@@ -32,7 +32,7 @@ crossroads.add({
 });
 
 
-//02 - params
+//02 - params => ugly
 
 //params => id:String, pattern:String, [callback]:Function, [rules]:Object
 crossroads.add('lipsum', 'lorem_{ipsum}', function(ipsum){
@@ -43,19 +43,19 @@ crossroads.add('lipsum', 'lorem_{ipsum}', function(ipsum){
 );
 
 
-//03 - simplified params + public properties
+//03 - simplified params + public properties - probably most popular usage *** FAVORITE ***
 var foo = crossroads.add('/{foo}/{bar}', myAwesomeCallback);
 foo.id = 'foobar';
 
 
-//04 - signals + public properties *** FAVORITE ***
+//04 - signals + public properties - flexible (multiple listeners) *** FAVORITE ***
 var bar = crossroads.addRoute('/{foo}');
 bar.separator = '-'; //will match: [/lorem/, /lorem/ipsum/dolor, /lorem_ipsum_dolor], won't match: [/lorem-, /lorem-ipsum/dolor]
 bar.matched.add(myAwesomeListener);
 
 
 
-//05 - "classical"
+//05 - "classical" => uncool, overly verbose.. no need to create a route without adding it to the router.
 var a = new crossroads.Route('/{foo}/{bar}/{ipsum}', myCallback);
 a.rules = {
 	bar : crossroads.INT,
@@ -73,14 +73,14 @@ crossroads.addRoute(a);
 crossroads.route('/foo/5/102'); //will trigger any route that matches this pattern 
 
 
-//02 - id + segments *** FAVORITE ***
+//02 - id + segments => easy to pass parameters in case you forget order, not a huge fan of strings for ids tho.. *** FAVORITE ***
 //params => routeId:string, [segments]:Object
 crossroads.route('foobar', {
 	bar : 5,
 	ipsum : 102
 });
 
-//02_2 - route + segments *** FAVORITE ***
+//02_2 - route + segments => if method accepts a string it should also accept a reference to the route..  *** FAVORITE ***
 //params => routeId:string, [segments]:Object
 crossroads.route(myRoute, {
 	bar : 5,
@@ -88,7 +88,7 @@ crossroads.route(myRoute, {
 });
 
 
-//03 - config object
+//03 - config object => unnecessary
 crossroads.trigger({
 	id : 'foobar',
 	segments : {
@@ -100,7 +100,7 @@ crossroads.trigger({
 
 //====== Get Route ======//
 
-//01 - *** FAVORITE ***
+//01 - I can't see myself using the ID but it may be useful for someone... *** FAVORITE ***
 var myRoute = crossroads.getRouteById('foobar');
 myRoute.matched.add(myOtherCallback);
 
@@ -125,9 +125,21 @@ myRoute.dispose();
 
 crossroads.notMatched.add(listener);
 crossroads.notFound.add(listener);
-crossroads.notRouted.add(listener);
-crossroads.routed.add(listener);
-crossroads.failed.add(listener);
-crossroads.bypassed.add(listener); // *** FAVORITE ***
+crossroads.notRouted.add(listener); //strange
+crossroads.routed.add(listener); // maybe keep this one, can be useful in some weird scenarios *** FAVORITE ***
+crossroads.failed.add(listener); //not matching a route isn't a failure
+crossroads.bypassed.add(listener); // simple name *** FAVORITE ***
 
 crossroads.setDefault(listener);
+
+
+
+//====== Config ======//
+
+crossroads.openTag = '{';
+crossroads.closeTag = '}';
+
+crossroads.paramRegexp = /{[^}]+}/; //no need to create new regexp object every time, harder to customize but not that much.. *** FAVORITE ***
+
+crossroads.separator = '/'; //used to divide route segments *** FAVORITE ***
+
