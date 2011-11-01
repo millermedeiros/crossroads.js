3a0773e89afe8a0b2dae953e4d0a584711b62bb7
millermedeiros <miller@millermedeiros.com>
2011-11-01 16:29:11 -0200

    added greedy routes support. see #20.

--- dev/src/crossroads.js
+++ dev/src/crossroads.js
@@ -46,12 +46,18 @@
         parse : function (request) {
             request = request || '';
 
-            var route = this._getMatchedRoute(request),
-                params = route? route._getParamsArray(request) : null;
+            var routes = this._getMatchedRoutes(request),
+                i = 0,
+                n = routes.length,
+                cur;
 
-            if (route) {
-                route.matched.dispatch.apply(route.matched, params);
-                this.routed.dispatch(request, route, params);
+            if (n) {
+                while (i < n) {
+                    cur = routes[i];
+                    cur.route.matched.dispatch.apply(cur.route.matched, cur.params);
+                    this.routed.dispatch(request, cur.route, cur.params, !i);
+                    i += 1;
+                }
             } else {
                 this.bypassed.dispatch(request);
             }
@@ -69,17 +75,21 @@
             routes.splice(n+1, 0, route);
         },
 
-        _getMatchedRoute : function (request) {
-            var routes = this._routes,
+        _getMatchedRoutes : function (request) {
+            var res = [],
+                routes = this._routes,
                 n = routes.length,
                 route;
             //should be decrement loop since higher priorities are added at the end of array
             while (route = routes[--n]) {
-                if (route.match(request)) {
-                    return route;
+                if ((!res.length || route.greedy) && route.match(request)) {
+                    res.push({
+                        route : route,
+                        params : route._getParamsArray(request)
+                    });
                 }
             }
-            return null;
+            return res;
         },
 
         toString : function () {
--- dev/tests/spec/signals.spec.js
+++ dev/tests/spec/signals.spec.js
@@ -10,6 +10,8 @@ describe('crossroads Signals', function(){
 
     afterEach(function(){
         crossroads.removeAllRoutes();
+        crossroads.bypassed.removeAll();
+        crossroads.routed.removeAll();
     });
 
 
@@ -34,23 +36,25 @@ describe('crossroads Signals', function(){
         expect( count ).toBe( 2 );
     });
 
+
     it('should dispatch routed at each match', function(){
-        var count = 0, 
-            requests = [], 
+        var count = 0,
+            requests = [],
             count2 = 0,
-            routed;
+            routed,
+            first;
 
         var a = crossroads.addRoute('/{foo}_{bar}');
         a.matched.add(function(foo, bar){
-            count2++;
+            count++;
         });
 
         crossroads.bypassed.add(function(request){
             requests.push(request);
-            count++;
+            count2++;
         });
 
-        crossroads.routed.add(function(request, route, params){
+        crossroads.routed.add(function(request, route, params, isFirst){
             requests.push(request);
             count++;
 
@@ -59,6 +63,7 @@ describe('crossroads Signals', function(){
             expect( params[0] ).toEqual( 'foo' );
             expect( params[1] ).toEqual( 'bar' );
             routed = true;
+            first = isFirst;
         });
 
         crossroads.parse('/lorem/ipsum');
@@ -69,10 +74,33 @@ describe('crossroads Signals', function(){
         expect( count ).toBe( 2 );
         expect( count2 ).toBe( 1 );
         expect( routed ).toEqual( true );
+        expect( first ).toEqual( true );
+
+    });
 
 
-        crossroads.bypassed.removeAll();
-        crossroads.routed.removeAll();
+    it('isFirst should be false on greedy matches', function () {
+
+        var count = 0,
+            firsts = [];
+
+        crossroads.routed.add(function(req, route, params, isFirst){
+            count += 1;
+            firsts.push(isFirst);
+        });
+
+        //anti-pattern!
+        crossroads.addRoute('/{a}/{b}');
+        crossroads.addRoute('/{a}/{b}').greedy = true;
+        crossroads.addRoute('/{a}/{b}').greedy = true;
+
+        crossroads.parse('/foo/bar');
+
+        expect( count ).toEqual( 3 );
+        expect( firsts[0] ).toEqual( true );
+        expect( firsts[1] ).toEqual( false );
+        expect( firsts[2] ).toEqual( false );
+
     });
 
 });
